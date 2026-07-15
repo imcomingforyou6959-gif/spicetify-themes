36458d458f43deadcf0aac498668aa222f8d3c59
onlyonestands <imcomingforyou6959@gmail.com>
2026-07-14 18:33:55 -0700

    Add theme configuration for Lonely theme

--- Lonely/theme.js
+++ Lonely/theme.js
@@ -1,1 +1,9 @@
+const theme = {
+    name: "Lonely",
+    author: "6x94 on discord",
+    version: "1.0.0",
+    inherits: "Default",
+    description: "Lonely Snow made just for u with a clean background and more",
+};
 
+module.exports = theme;
